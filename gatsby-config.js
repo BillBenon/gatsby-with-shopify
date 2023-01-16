d5bc7adbbc01439eea4cb09abebcb88e66756faa
Dushime Bill Benon <DushimeBillBenon@gmail.com>
2023-01-16 20:34:42 +0200

    init: working project

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: "gatsby-starter-shopify",
+    siteTitleDefault: "gatsby-starter-shopify by @GatsbyJS",
+    siteUrl: "https://shopify-demo.gatsbyjs.com",
+    siteDescription:
+      "A Gatsby starter using the latest Shopify plugin showcasing a store with product overview, individual product pages, and a cart.",
+    siteImage: "/default-og-image.jpg",
+    twitter: "@gatsbyjs",
+  },
+  flags: {
+    FAST_DEV: true,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        password: "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",
+        storeUrl: "sample-gatsby.myshopify.com",
+        shopifyConnections: ["collections"],
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-gatsby-cloud",
+    // Add your Google Analytics ID to the .env file to enable
+    // Otherwise, this plugin can be removed
+    process.env.GOOGLE_ANALYTICS_ID && {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+  ].filter(Boolean),
+}
